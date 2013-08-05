ca3d0d146324d1152586c90188ee8a13c0af025d
scapella <salcagu@gmail.com>
2013-08-05 11:43:40 +0200

    Added to the script for the random selection of species two ways to make such selection. The first one is purely random and the second one make a random selection of species per group considering the size of each group.

--- source/01.RandomSelectionSpeciesSets.py
+++ source/01.RandomSelectionSpeciesSets.py
@@ -30,14 +30,23 @@ def main(argv):
     required = True, help = "Seed species. Seed species will be for sure in the"
     + " training set")
 
-  group = parser.add_mutually_exclusive_group(required = True)
+  group_size = parser.add_mutually_exclusive_group(required = True)
 
-  group.add_argument("--absolut_size", dest = "absolutSize", default = -1,
+  group_size.add_argument("--absolut_size", dest = "absolutSize", default = -1,
     type = int, help = "Absolut size of training set of species")
 
-  group.add_argument("--relative_size", dest = "relativSize", default = -1,
+  group_size.add_argument("--relative_size", dest = "relativSize", default = -1,
     type = float, help = "Relative sive of the training set of species "
     + "regarding the total input species")
+
+  group_sel = parser.add_mutually_exclusive_group(required = True)
+
+  group_sel.add_argument("--random", dest = "random", default = False, action =\
+    "store_true", help = "Random selection of species for the training set")
+
+  group_sel.add_argument("--guided", dest = "guided", default = False, action =\
+    "store_true", help = "Training set is composed by randomly selected species"
+    + " in proportion to each group size")
 
   parser.add_argument("-o", "--out", dest = "outFile", type = str, default = "",
     help = "Output file")
@@ -73,19 +82,38 @@ def main(argv):
   trainingSize = args.absolutSize if args.absolutSize != -1 else \
     int(round(n * args.relativSize))
 
-  ## Build a structure to make an "efficient" random selection of species based
-  ## on groups
+  ## Depending on the strategy selected for randomly construct the training set,
+  ## build an "efficient" structure (for sure it could be improved) which will
+  ## facilitate the process of selection
   all_species = {}
   for gr in species:
-    key_1 = len(all_species)
+    key_1 = 0 if args.random else len(all_species)
     all_species.setdefault(key_1, {})
     for sp in species[gr]:
       all_species[key_1].setdefault(len(all_species[key_1]), ("%s|%s") % (gr,sp))
 
-  while len(training) < trainingSize:
-    key_1 = randint(0, len(all_species) - 1)
-    key_2 = randint(0, len(all_species[key_1]) - 1)
-    training.add(all_species[key_1][key_2])
+  ## For each group, select random species up to the limit assigned to the group
+  ## On the "random" strategy, there is just one group so the limit for this
+  ## unique group is the size of the whole training set
+  acum = 0
+  for gr in all_species:
+
+    ## Determine how many species should be selected from current set
+    size = int(round((float(trainingSize)/n) * len(all_species[gr])))
+    size = size if (acum + size) < trainingSize else (trainingSize - acum)
+    acum += size
+
+    ## Substract the number of seeds belonging to the current group which have
+    ## been already included into the training set
+    size -= len(set([all_species[gr][e] for e in all_species[gr]]) & training)
+
+    groupSize = len(all_species[gr]) - 1
+    ## Select randomly as many species as asssigned to the group
+    while size > 0:
+      key = randint(0, groupSize)
+      if not all_species[gr][key] in training:
+        training.add(all_species[gr][key])
+        size -= 1
 
   oFile = sys.stdout if not args.outFile else open(args.outFile, "w")
   ## Print information about the general process
@@ -98,7 +126,7 @@ def main(argv):
   ## Rest of member in the training set
   for e in sorted([e for e in training if not e.split("|")[1] in args.seedSps]):
     gr, sp = e.split("|")
-    print >> oFile, ("Training\t%-4s\t%s") % ("", "\t".join(species[gr][sp]))
+    print >> oFile, ("Training\t%s\t%s") % ("", "\t".join(species[gr][sp]))
 
   print >> oFile, ("## Testing set size: %d") % (n - trainingSize)
   ## Member in the testing set:
@@ -106,7 +134,7 @@ def main(argv):
     for sp in sorted(species[gr]):
       if ("%s|%s") % (gr, sp) in training:
         continue
-      print >> oFile, ("Testing \t%-4s\t%s") % ("", "\t".join(species[gr][sp]))
+      print >> oFile, ("Testing \t%s\t%s") % ("", "\t".join(species[gr][sp]))
   oFile.close()
 ## ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** *****
 if __name__ == "__main__":
