1cded5cb851d8e8f771687c1786609c6be3ff689
scapella <salcagu@gmail.com>
2013-08-08 13:04:17 +0200

    Added two new scripts to:   1) Generate in which order the individual marker genes should be concatenated according to one or more given score/s. Score/s can be sorted, individually, on ascending or descending order.   2) Measure the degree of overlap among different runs using the same or different sets of species during the training phase. On the case of different sets of species, the script compute the overlap based on the common subset of species.
    Added an example about how it should be the input file for script '08.GenerateProgressiveConcatenationList.py'

---
+++ source/08.GenerateProgressiveConcatenationList.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+## ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** *****
+##
+## Example how to call this script using available examples:
+## python 08.GenerateProgressiveConcatenationList.py --column 2 6 --sort asc \
+## desc --in ../examples/08.SingleMarkers_vs_Reference --verbose
+##
+##
+import sys, os, argparse
+from string import strip
+
+def main(argv):
+
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument("-i", "--in", dest = "inFile", type = str, required = True,
+    help = "Input file containing individual scores to decide how individual "
+    + "markers will be progressively concatenated")
+
+  parser.add_argument("--column", dest = "columns", type = int, nargs = '+',
+    default = [], help = "Columns in the input file containing scores to decide"
+    + " the order of the progressive concatenation. It may contain more than "
+    + "one column. Starts at 0")
+
+  parser.add_argument("--sort", dest = "sortColumns", choices = ["asc", "desc"],
+    default = [], type = str, nargs = '+', help = "Sort strategy for each score"
+    + " column")
+
+  parser.add_argument("--delim", dest = "delim", type = str, default = "\t",
+    help = "Delimiter e.g. <tab> of fields in the input file")
+
+  parser.add_argument("--column_id", dest = "idColumn", type = int, default = 0,
+    help = "Column containing the different marker gene IDs. Starts at 0.")
+
+  parser.add_argument("-o", "--out", dest = "outFile", type = str, default = "",
+    help = "Output file")
+
+  parser.add_argument("--verbose", dest = "verbose", default = False, action = \
+    "store_true", help = "Active verbosity to see how entry are sorted")
+
+  args = parser.parse_args()
+
+  ## Check if the input files have been well defined.
+  if not os.path.isfile(args.inFile):
+    sys.exit(("ERROR: Check input file '%s'")  % (args.inFile))
+
+  ## Check there is a sorting strategy for each column scores
+  if len(args.columns) != len(args.sortColumns):
+    sys.exit(("ERROR: Each score column should have defining its sorting order"))
+
+  data = []
+  for line in open(args.inFile, "rU"):
+    f = map(strip, line.split(args.delim))
+    data.append([int(f[args.columns[p]]) * (1 if args.sortColumns[p] == "asc" \
+      else -1) for p in range(len(args.columns))] + [f[args.idColumn]])
+
+  oFile = open(args.outFile, "w") if args.outFile else sys.stdout
+
+  if args.verbose:
+    print "\n".join(map(str, sorted(data)))
+
+  current = [sorted(data)[0][-1]]
+  for entry in sorted(data)[1:]:
+    current.append(entry[-1])
+    print >> oFile, ("%s\t%d\t%s") % (str(len(current)).zfill(4), len(current),
+      ",".join(sorted(current)))
+  oFile.close()
+### ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ****
+if __name__ == "__main__":
+  sys.exit(main(sys.argv[1:]))
---
+++ source/16.MeasuringOverlap.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+## ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** *****
+##
+## Example how to call this script using available examples:
+##
+## python 05.GetMarkersCandidates.py -i ../examples/05.input_filtered_blast \
+## --low_limit 55
+##
+import sys, os, argparse
+from string import strip
+from hashlib import md5
+
+def main(argv):
+
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument("-i", "--in", dest = "files", type = str, required = True,
+    nargs = '+', default = [], help = "Input file/s containing Marker Gene "
+    + "candidates which will be fused into one single list")
+
+  parser.add_argument("--column", dest = "column", type = int, default = 2,
+    help = "Column containing sets of homologous sequences. Starts at 0")
+
+  parser.add_argument("--separator", dest = "separator", type = str, default = \
+    ",", help = "Separator symbol e.g. ',' used to separate sequences")
+
+  parser.add_argument("--delim", dest = "delim", type = str, default = "\t",
+    help = "Delimiter e.g. <tab> of fields in the input file")
+
+  parser.add_argument("--strategy", dest = "strategy", default = "", choices = \
+    ["all", "common", "uncommon"], help = "Print all, just overlapping cases or"
+    + " not overlapping cases")
+
+  parser.add_argument("-o", "--out", dest = "outFile", type = str, default = "",
+    help = "Output file")
+
+  args = parser.parse_args()
+
+  ## Check if the input files have been well defined.
+  for inFile in args.files:
+    if not os.path.isfile(inFile):
+      sys.exit(("ERROR: Check input Marker Genes file '%s'")  % (inFile))
+
+  input_data, species = {}, {}
+  for inFile in args.files:
+    species.setdefault(inFile, set())
+    for line in open(inFile, "rU"):
+      f = map(strip, line.split(args.delim))
+      sequences = f[args.column].split(args.separator)
+      species[inFile] |= set([s[:3] if s.find("_") == -1 else s.split("_")[1] \
+        for s in sequences])
+      input_data.setdefault(inFile, []).append(f)
+
+  numberDatasets = len(args.files)
+  common = [sp for d in species for sp in species[d]]
+  common = set([s for s in set(common) if common.count(s) == numberDatasets])
+  uncommon = set([sp for d in species for sp in species[d] if not sp in common])
+
+  data = {}
+  for dataset in input_data:
+    for line in input_data[dataset]:
+      subset = []
+      for seq in line[args.column].split(args.separator):
+        sp = seq[:3] if seq.find("_") == -1 else seq.split("_")[1]
+        if sp in common:
+          subset.append(seq)
+      key = md5(",".join(sorted(subset))).hexdigest()
+      entry = [("[%s]") % (dataset)] + line
+      data.setdefault(key, set()).add(args.delim.join(entry))
+
+  print ("## Common Species:     %d") % (len(common))
+  print ("## Non-Common Species: %d") % (len(uncommon))
+  print ("## Total Species:      %d\n") % (len(common) + len(uncommon))
+  print ("## All Markers:        %d") % (len(data))
+  common = len([d for d in data if len(data[d]) == numberDatasets])
+  print ("## Common Markers:     %d") % (common)
+
+  oFile = open(args.outFile, "w") if args.outFile else sys.stdout
+  ## Print, depending on input parameters, the overlapping and non-overlapping
+  ## datasets
+  print args.strategy
+  if args.strategy in ["common", "all"]:
+    for ref in [d for d in data if len(data[d]) == numberDatasets]:
+      for line in sorted(data[ref]):
+        print >> oFile, line
+
+  if args.strategy in ["uncommon", "all"]:
+    for ref in [d for d in data if data[d] != numberDatasets]:
+      for line in sorted(data[ref]):
+        print >> oFile, ("[%s]%s%s") % (ref, args.delim, line)
+  oFile.close()
+### ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ****
+if __name__ == "__main__":
+  sys.exit(main(sys.argv[1:]))
